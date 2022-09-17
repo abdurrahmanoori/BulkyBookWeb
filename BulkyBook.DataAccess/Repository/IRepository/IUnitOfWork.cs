0f854fec1711d50a89926c01f36180f5cb50f009
Abdurrahman <Abdurrahmannoori10@gmail.com>
2022-09-17 12:16:52 +0430

    IUnitOfWork interface is declared and also implented UnitOfWork.

---
+++ BulkyBook.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BulkyBook.DataAccess.Repository.IRepository
+{
+    public interface IUnitOfWork
+    {
+        ICategoryRepository Category { get; }
+        void Save();
+
+
+
+
+
+
+
+
+
+        //ICoverTypeRepository CoverType { get; }
+        //IProductRepository Product { get; }
+        //ICompanyRepository Company { get; }
+        //IApplicationUserRepository ApplicationUser { get; }
+        //IShoppingCartRepository ShoppingCart { get; }
+        //IOrderDetailRepository OrderDetail { get; }
+        //IOrderHeaderRepository OrderHeader { get; }
+
+    }
+}
---
+++ BulkyBook.DataAccess/Repository/UnitOfWork.cs
@@ -0,0 +1,26 @@
+﻿using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBookWeb.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BulkyBook.DataAccess.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _db;
+
+        public UnitOfWork(ApplicationDbContext db)
+        {
+            _db = db;
+            Category = new CategoryRepository(_db);
+        }
+        public ICategoryRepository Category { get; private set; }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+
+    }
+}
